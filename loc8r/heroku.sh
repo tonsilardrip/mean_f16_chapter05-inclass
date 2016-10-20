42df19ae907a6a92eb5a3e448c2e472bebffacf2
tonsilardrip <cshoemake2226@gmail.com>
2016-10-20 02:09:35 +0000

    first commit

---
+++ loc8r/heroku.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# helpful: http://stackoverflow.com/questions/8482843/git-commit-bash-script
+git add .
+read -p "Commit description: " desc
+git commit -m "$desc"
+git push heroku master
